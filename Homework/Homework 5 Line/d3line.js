75cf50813f33b4e5bcb5b4ad76bb962ca03956c9
JasperLe <lelijveldj@hotmail.com>
2016-11-30 21:01:31 +0100

    Add files via upload
    Introductie homework d3line

---
+++ Homework/Homework 5 Line/d3line.js
@@ -0,0 +1,12 @@
+/* auteur: Jasper Lelijveld
+* studentnummer: 11401753
+* vak: Data Processing
+* Week 5 - d3Line
+*/
+d3.json("d3line.json", function(data){
+    var obj = data;
+    var Leeuwarden = obj[0];
+    var Maastricht = obj[1];
+
+    console.log(data);
+});
